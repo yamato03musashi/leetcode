c2bd5b62364e68acb466b0106b432b1785e5e64b
sekine <nc0uhxa756@gmail.com>
2024-11-12 12:06:58 +0900

    add comment

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -12,18 +12,19 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # ダミーノードを作成し、現在のポインタ(cur)として使用
         cur = dummy = ListNode()
-        while list1 and list2:               
+
+        while list1 and list2:
             if list1.val < list2.val:
-                cur.next = list1
-                list1, cur = list1.next, list1
+                cur.next = list1 # 現在のノードの次をlist1に設定
+                cur = list1 # curを更新
+                list1 = list1.next # list1を次に進め
             else:
                 cur.next = list2
-                list2, cur = list2.next, list2
-                
-        if list1 or list2:
-            cur.next = list1 if list1 else list2
-            
-        return dummy.next
+                cur = list2
+                list2 = list2.next
+        
+
 # @lc code=end
 
