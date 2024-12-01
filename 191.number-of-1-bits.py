ab262c6164839014ea660651dbe6a940445f6002
sekine <nc0uhxa756@gmail.com>
2024-12-02 00:07:38 +0900

    second submit

--- 191.number-of-1-bits.py
+++ 191.number-of-1-bits.py
@@ -8,9 +8,9 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
-        while n != 0:
-            n &= (n - 1)
-            count += 1
+        while n != 0: 
+            count += n & 1
+            n >>= 1
         return count
         
 # @lc code=end
