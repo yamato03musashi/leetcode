b55db7f3652d03d8a9d8812bcfcdd7c749c87b2b
sekine <nc0uhxa756@gmail.com>
2024-12-06 23:41:41 +0900

    first submit

---
+++ 217.contains-duplicate.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=217 lang=python3
+#
+# [217] Contains Duplicate
+#
+
+# @lc code=start
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
+# @lc code=end
+
