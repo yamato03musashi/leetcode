ff8ccd8e7f6ca014c92f4e656e448262039297c8
sekine <nc0uhxa756@gmail.com>
2024-12-11 23:52:33 +0900

    add comment

--- 232.implement-queue-using-stacks.py
+++ 232.implement-queue-using-stacks.py
@@ -5,26 +5,36 @@
 #
 
 # @lc code=start
-class MyQueue(object):
+class MyQueue:
     def __init__(self):
-        self.in_stk = []
-        self.out_stk = []
-	# Push element x to the back of queue...
-    def push(self, x):
-        self.in_stk.append(x)
-	# Remove the element from the front of the queue and returns it...
-    def pop(self):
-        self.peek()
-        return self.out_stk.pop()
-	# Get the front element...
-    def peek(self):
-        if not self.out_stk:
-            while self.in_stk:
-                self.out_stk.append(self.in_stk.pop())
-        return self.out_stk[-1]
-	# Return whether the queue is empty...
-    def empty(self):
-        return not self.in_stk and not self.out_stk
+        # メインスタック：要素の追加用
+        self.stack1 = []
+        # 補助スタック：要素の取り出し用
+        self.stack2 = []
+        
+    def push(self, x: int) -> None:
+        # 新しい要素は常にstack1に追加
+        self.stack1.append(x)
+    
+    def _transfer(self) -> None:
+        # stack2が空の場合のみ、stack1の要素を全てstack2に移動
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+    
+    def pop(self) -> int:
+        # stack2にデータを移動し、先頭要素を取り出す
+        self._transfer()
+        return self.stack2.pop()
+    
+    def peek(self) -> int:
+        # stack2にデータを移動し、先頭要素を参照する
+        self._transfer()
+        return self.stack2[-1]
+    
+    def empty(self) -> bool:
+        # 両方のスタックが空の場合、キューは空
+        return len(self.stack1) == 0 and len(self.stack2) == 0
 
 
 # Your MyQueue object will be instantiated and called as such:
